409395b0366b5345139afd558b62b1966ca1165a
Tomas Peirotti <tomas.peirotti@gmail.com>
2020-11-16 23:08:49 -0300

    App version uploaded

---
+++ public/assets/static-data/data.ts
@@ -0,0 +1,843 @@
+export var deathProb = [
+  {
+    age: '0',
+    maleDeathProb: '0.006304',
+    maleLifeExpectancy: '75.97',
+    femaleDeathProb: '0.005229',
+    femaleLifeExpectancy: '80.96'
+  },
+  {
+    age: '1',
+    maleDeathProb: '0.000426',
+    maleLifeExpectancy: '75.45',
+    femaleDeathProb: '0.000342',
+    femaleLifeExpectancy: '80.39'
+  },
+  {
+    age: '2',
+    maleDeathProb: '0.000290',
+    maleLifeExpectancy: '74.48',
+    femaleDeathProb: '0.000209',
+    femaleLifeExpectancy: '79.42'
+  },
+  {
+    age: '3',
+    maleDeathProb: '0.000229',
+    maleLifeExpectancy: '73.50',
+    femaleDeathProb: '0.000162',
+    femaleLifeExpectancy: '78.43'
+  },
+  {
+    age: '4',
+    maleDeathProb: '0.000162',
+    maleLifeExpectancy: '72.52',
+    femaleDeathProb: '0.000143',
+    femaleLifeExpectancy: '77.45'
+  },
+  {
+    age: '5',
+    maleDeathProb: '0.000146',
+    maleLifeExpectancy: '71.53',
+    femaleDeathProb: '0.000125',
+    femaleLifeExpectancy: '76.46'
+  },
+  {
+    age: '6',
+    maleDeathProb: '0.000136',
+    maleLifeExpectancy: '70.54',
+    femaleDeathProb: '0.000113',
+    femaleLifeExpectancy: '75.47'
+  },
+  {
+    age: '7',
+    maleDeathProb: '0.000127',
+    maleLifeExpectancy: '69.55',
+    femaleDeathProb: '0.000104',
+    femaleLifeExpectancy: '74.47'
+  },
+  {
+    age: '8',
+    maleDeathProb: '0.000115',
+    maleLifeExpectancy: '68.56',
+    femaleDeathProb: '0.000097',
+    femaleLifeExpectancy: '73.48'
+  },
+  {
+    age: '9',
+    maleDeathProb: '0.000103',
+    maleLifeExpectancy: '67.57',
+    femaleDeathProb: '0.000093',
+    femaleLifeExpectancy: '72.49'
+  },
+  {
+    age: '10',
+    maleDeathProb: '0.000097',
+    maleLifeExpectancy: '66.57',
+    femaleDeathProb: '0.000092',
+    femaleLifeExpectancy: '71.50'
+  },
+  {
+    age: '11',
+    maleDeathProb: '0.000109',
+    maleLifeExpectancy: '65.58',
+    femaleDeathProb: '0.000098',
+    femaleLifeExpectancy: '70.50'
+  },
+  {
+    age: '12',
+    maleDeathProb: '0.000151',
+    maleLifeExpectancy: '64.59',
+    femaleDeathProb: '0.000113',
+    femaleLifeExpectancy: '69.51'
+  },
+  {
+    age: '13',
+    maleDeathProb: '0.000232',
+    maleLifeExpectancy: '63.60',
+    femaleDeathProb: '0.000138',
+    femaleLifeExpectancy: '68.52'
+  },
+  {
+    age: '14',
+    maleDeathProb: '0.000343',
+    maleLifeExpectancy: '62.61',
+    femaleDeathProb: '0.000172',
+    femaleLifeExpectancy: '67.53'
+  },
+  {
+    age: '15',
+    maleDeathProb: '0.000465',
+    maleLifeExpectancy: '61.63',
+    femaleDeathProb: '0.000211',
+    femaleLifeExpectancy: '66.54'
+  },
+  {
+    age: '16',
+    maleDeathProb: '0.000588',
+    maleLifeExpectancy: '60.66',
+    femaleDeathProb: '0.000251',
+    femaleLifeExpectancy: '65.55'
+  },
+  {
+    age: '17',
+    maleDeathProb: '0.000720',
+    maleLifeExpectancy: '59.70',
+    femaleDeathProb: '0.000293',
+    femaleLifeExpectancy: '64.57'
+  },
+  {
+    age: '18',
+    maleDeathProb: '0.000858',
+    maleLifeExpectancy: '58.74',
+    femaleDeathProb: '0.000336',
+    femaleLifeExpectancy: '63.59'
+  },
+  {
+    age: '19',
+    maleDeathProb: '0.000999',
+    maleLifeExpectancy: '57.79',
+    femaleDeathProb: '0.000379',
+    femaleLifeExpectancy: '62.61'
+  },
+  {
+    age: '20',
+    maleDeathProb: '0.001146',
+    maleLifeExpectancy: '56.85',
+    femaleDeathProb: '0.000425',
+    femaleLifeExpectancy: '61.63'
+  },
+  {
+    age: '21',
+    maleDeathProb: '0.001288',
+    maleLifeExpectancy: '55.91',
+    femaleDeathProb: '0.000472',
+    femaleLifeExpectancy: '60.66'
+  },
+  {
+    age: '22',
+    maleDeathProb: '0.001407',
+    maleLifeExpectancy: '54.98',
+    femaleDeathProb: '0.000515',
+    femaleLifeExpectancy: '59.69'
+  },
+  {
+    age: '23',
+    maleDeathProb: '0.001494',
+    maleLifeExpectancy: '54.06',
+    femaleDeathProb: '0.000551',
+    femaleLifeExpectancy: '58.72'
+  },
+  {
+    age: '24',
+    maleDeathProb: '0.001556',
+    maleLifeExpectancy: '53.14',
+    femaleDeathProb: '0.000582',
+    femaleLifeExpectancy: '57.75'
+  },
+  {
+    age: '25',
+    maleDeathProb: '0.001610',
+    maleLifeExpectancy: '52.22',
+    femaleDeathProb: '0.000612',
+    femaleLifeExpectancy: '56.78'
+  },
+  {
+    age: '26',
+    maleDeathProb: '0.001665',
+    maleLifeExpectancy: '51.31',
+    femaleDeathProb: '0.000646',
+    femaleLifeExpectancy: '55.82'
+  },
+  {
+    age: '27',
+    maleDeathProb: '0.001717',
+    maleLifeExpectancy: '50.39',
+    femaleDeathProb: '0.000684',
+    femaleLifeExpectancy: '54.85'
+  },
+  {
+    age: '28',
+    maleDeathProb: '0.001767',
+    maleLifeExpectancy: '49.48',
+    femaleDeathProb: '0.000729',
+    femaleLifeExpectancy: '53.89'
+  },
+  {
+    age: '29',
+    maleDeathProb: '0.001817',
+    maleLifeExpectancy: '48.56',
+    femaleDeathProb: '0.000779',
+    femaleLifeExpectancy: '52.93'
+  },
+  {
+    age: '30',
+    maleDeathProb: '0.001865',
+    maleLifeExpectancy: '47.65',
+    femaleDeathProb: '0.000833',
+    femaleLifeExpectancy: '51.97'
+  },
+  {
+    age: '31',
+    maleDeathProb: '0.001911',
+    maleLifeExpectancy: '46.74',
+    femaleDeathProb: '0.000887',
+    femaleLifeExpectancy: '51.01'
+  },
+  {
+    age: '32',
+    maleDeathProb: '0.001960',
+    maleLifeExpectancy: '45.83',
+    femaleDeathProb: '0.000939',
+    femaleLifeExpectancy: '50.06'
+  },
+  {
+    age: '33',
+    maleDeathProb: '0.002014',
+    maleLifeExpectancy: '44.92',
+    femaleDeathProb: '0.000988',
+    femaleLifeExpectancy: '49.10'
+  },
+  {
+    age: '34',
+    maleDeathProb: '0.002071',
+    maleLifeExpectancy: '44.01',
+    femaleDeathProb: '0.001034',
+    femaleLifeExpectancy: '48.15'
+  },
+  {
+    age: '35',
+    maleDeathProb: '0.002138',
+    maleLifeExpectancy: '43.10',
+    femaleDeathProb: '0.001085',
+    femaleLifeExpectancy: '47.20'
+  },
+  {
+    age: '36',
+    maleDeathProb: '0.002211',
+    maleLifeExpectancy: '42.19',
+    femaleDeathProb: '0.001143',
+    femaleLifeExpectancy: '46.25'
+  },
+  {
+    age: '37',
+    maleDeathProb: '0.002279',
+    maleLifeExpectancy: '41.28',
+    femaleDeathProb: '0.001205',
+    femaleLifeExpectancy: '45.30'
+  },
+  {
+    age: '38',
+    maleDeathProb: '0.002342',
+    maleLifeExpectancy: '40.37',
+    femaleDeathProb: '0.001271',
+    femaleLifeExpectancy: '44.36'
+  },
+  {
+    age: '39',
+    maleDeathProb: '0.002405',
+    maleLifeExpectancy: '39.47',
+    femaleDeathProb: '0.001345',
+    femaleLifeExpectancy: '43.41'
+  },
+  {
+    age: '40',
+    maleDeathProb: '0.002482',
+    maleLifeExpectancy: '38.56',
+    femaleDeathProb: '0.001429',
+    femaleLifeExpectancy: '42.47'
+  },
+  {
+    age: '41',
+    maleDeathProb: '0.002583',
+    maleLifeExpectancy: '37.65',
+    femaleDeathProb: '0.001524',
+    femaleLifeExpectancy: '41.53'
+  },
+  {
+    age: '42',
+    maleDeathProb: '0.002710',
+    maleLifeExpectancy: '36.75',
+    femaleDeathProb: '0.001630',
+    femaleLifeExpectancy: '40.59'
+  },
+  {
+    age: '43',
+    maleDeathProb: '0.002870',
+    maleLifeExpectancy: '35.85',
+    femaleDeathProb: '0.001748',
+    femaleLifeExpectancy: '39.66'
+  },
+  {
+    age: '44',
+    maleDeathProb: '0.003064',
+    maleLifeExpectancy: '34.95',
+    femaleDeathProb: '0.001881',
+    femaleLifeExpectancy: '38.73'
+  },
+  {
+    age: '45',
+    maleDeathProb: '0.003285',
+    maleLifeExpectancy: '34.06',
+    femaleDeathProb: '0.002029',
+    femaleLifeExpectancy: '37.80'
+  },
+  {
+    age: '46',
+    maleDeathProb: '0.003538',
+    maleLifeExpectancy: '33.17',
+    femaleDeathProb: '0.002195',
+    femaleLifeExpectancy: '36.88'
+  },
+  {
+    age: '47',
+    maleDeathProb: '0.003834',
+    maleLifeExpectancy: '32.28',
+    femaleDeathProb: '0.002386',
+    femaleLifeExpectancy: '35.96'
+  },
+  {
+    age: '48',
+    maleDeathProb: '0.004178',
+    maleLifeExpectancy: '31.41',
+    femaleDeathProb: '0.002605',
+    femaleLifeExpectancy: '35.04'
+  },
+  {
+    age: '49',
+    maleDeathProb: '0.004569',
+    maleLifeExpectancy: '30.54',
+    femaleDeathProb: '0.002851',
+    femaleLifeExpectancy: '34.13'
+  },
+  {
+    age: '50',
+    maleDeathProb: '0.004997',
+    maleLifeExpectancy: '29.67',
+    femaleDeathProb: '0.003118',
+    femaleLifeExpectancy: '33.23'
+  },
+  {
+    age: '51',
+    maleDeathProb: '0.005462',
+    maleLifeExpectancy: '28.82',
+    femaleDeathProb: '0.003403',
+    femaleLifeExpectancy: '32.33'
+  },
+  {
+    age: '52',
+    maleDeathProb: '0.005971',
+    maleLifeExpectancy: '27.98',
+    femaleDeathProb: '0.003714',
+    femaleLifeExpectancy: '31.44'
+  },
+  {
+    age: '53',
+    maleDeathProb: '0.006526',
+    maleLifeExpectancy: '27.14',
+    femaleDeathProb: '0.004052',
+    femaleLifeExpectancy: '30.55'
+  },
+  {
+    age: '54',
+    maleDeathProb: '0.007125',
+    maleLifeExpectancy: '26.32',
+    femaleDeathProb: '0.004415',
+    femaleLifeExpectancy: '29.68'
+  },
+  {
+    age: '55',
+    maleDeathProb: '0.007766',
+    maleLifeExpectancy: '25.50',
+    femaleDeathProb: '0.004813',
+    femaleLifeExpectancy: '28.81'
+  },
+  {
+    age: '56',
+    maleDeathProb: '0.008445',
+    maleLifeExpectancy: '24.70',
+    femaleDeathProb: '0.005233',
+    femaleLifeExpectancy: '27.94'
+  },
+  {
+    age: '57',
+    maleDeathProb: '0.009156',
+    maleLifeExpectancy: '23.90',
+    femaleDeathProb: '0.005647',
+    femaleLifeExpectancy: '27.09'
+  },
+  {
+    age: '58',
+    maleDeathProb: '0.009897',
+    maleLifeExpectancy: '23.12',
+    femaleDeathProb: '0.006043',
+    femaleLifeExpectancy: '26.24'
+  },
+  {
+    age: '59',
+    maleDeathProb: '0.010671',
+    maleLifeExpectancy: '22.34',
+    femaleDeathProb: '0.006441',
+    femaleLifeExpectancy: '25.39'
+  },
+  {
+    age: '60',
+    maleDeathProb: '0.011519',
+    maleLifeExpectancy: '21.58',
+    femaleDeathProb: '0.006886',
+    femaleLifeExpectancy: '24.56'
+  },
+  {
+    age: '61',
+    maleDeathProb: '0.012419',
+    maleLifeExpectancy: '20.83',
+    femaleDeathProb: '0.007391',
+    femaleLifeExpectancy: '23.72'
+  },
+  {
+    age: '62',
+    maleDeathProb: '0.013307',
+    maleLifeExpectancy: '20.08',
+    femaleDeathProb: '0.007931',
+    femaleLifeExpectancy: '22.90'
+  },
+  {
+    age: '63',
+    maleDeathProb: '0.014164',
+    maleLifeExpectancy: '19.35',
+    femaleDeathProb: '0.008508',
+    femaleLifeExpectancy: '22.07'
+  },
+  {
+    age: '64',
+    maleDeathProb: '0.015032',
+    maleLifeExpectancy: '18.62',
+    femaleDeathProb: '0.009142',
+    femaleLifeExpectancy: '21.26'
+  },
+  {
+    age: '65',
+    maleDeathProb: '0.016013',
+    maleLifeExpectancy: '17.89',
+    femaleDeathProb: '0.009874',
+    femaleLifeExpectancy: '20.45'
+  },
+  {
+    age: '66',
+    maleDeathProb: '0.017138',
+    maleLifeExpectancy: '17.18',
+    femaleDeathProb: '0.010717',
+    femaleLifeExpectancy: '19.65'
+  },
+  {
+    age: '67',
+    maleDeathProb: '0.018362',
+    maleLifeExpectancy: '16.47',
+    femaleDeathProb: '0.011660',
+    femaleLifeExpectancy: '18.86'
+  },
+  {
+    age: '68',
+    maleDeathProb: '0.019693',
+    maleLifeExpectancy: '15.77',
+    femaleDeathProb: '0.012711',
+    femaleLifeExpectancy: '18.07'
+  },
+  {
+    age: '69',
+    maleDeathProb: '0.021174',
+    maleLifeExpectancy: '15.07',
+    femaleDeathProb: '0.013894',
+    femaleLifeExpectancy: '17.30'
+  },
+  {
+    age: '70',
+    maleDeathProb: '0.022889',
+    maleLifeExpectancy: '14.39',
+    femaleDeathProb: '0.015285',
+    femaleLifeExpectancy: '16.54'
+  },
+  {
+    age: '71',
+    maleDeathProb: '0.024869',
+    maleLifeExpectancy: '13.71',
+    femaleDeathProb: '0.016878',
+    femaleLifeExpectancy: '15.79'
+  },
+  {
+    age: '72',
+    maleDeathProb: '0.027095',
+    maleLifeExpectancy: '13.05',
+    femaleDeathProb: '0.018607',
+    femaleLifeExpectancy: '15.05'
+  },
+  {
+    age: '73',
+    maleDeathProb: '0.029587',
+    maleLifeExpectancy: '12.40',
+    femaleDeathProb: '0.020466',
+    femaleLifeExpectancy: '14.32'
+  },
+  {
+    age: '74',
+    maleDeathProb: '0.032394',
+    maleLifeExpectancy: '11.76',
+    femaleDeathProb: '0.022522',
+    femaleLifeExpectancy: '13.61'
+  },
+  {
+    age: '75',
+    maleDeathProb: '0.035668',
+    maleLifeExpectancy: '11.14',
+    femaleDeathProb: '0.024929',
+    femaleLifeExpectancy: '12.92'
+  },
+  {
+    age: '76',
+    maleDeathProb: '0.039396',
+    maleLifeExpectancy: '10.53',
+    femaleDeathProb: '0.027729',
+    femaleLifeExpectancy: '12.23'
+  },
+  {
+    age: '77',
+    maleDeathProb: '0.043453',
+    maleLifeExpectancy: '9.94',
+    femaleDeathProb: '0.030855',
+    femaleLifeExpectancy: '11.57'
+  },
+  {
+    age: '78',
+    maleDeathProb: '0.047826',
+    maleLifeExpectancy: '9.37',
+    femaleDeathProb: '0.034321',
+    femaleLifeExpectancy: '10.92'
+  },
+  {
+    age: '79',
+    maleDeathProb: '0.052649',
+    maleLifeExpectancy: '8.82',
+    femaleDeathProb: '0.038211',
+    femaleLifeExpectancy: '10.29'
+  },
+  {
+    age: '80',
+    maleDeathProb: '0.058206',
+    maleLifeExpectancy: '8.28',
+    femaleDeathProb: '0.042771',
+    femaleLifeExpectancy: '9.68'
+  },
+  {
+    age: '81',
+    maleDeathProb: '0.064581',
+    maleLifeExpectancy: '7.76',
+    femaleDeathProb: '0.047992',
+    femaleLifeExpectancy: '9.09'
+  },
+  {
+    age: '82',
+    maleDeathProb: '0.071657',
+    maleLifeExpectancy: '7.26',
+    femaleDeathProb: '0.053678',
+    femaleLifeExpectancy: '8.52'
+  },
+  {
+    age: '83',
+    maleDeathProb: '0.079465',
+    maleLifeExpectancy: '6.79',
+    femaleDeathProb: '0.059810',
+    femaleLifeExpectancy: '7.98'
+  },
+  {
+    age: '84',
+    maleDeathProb: '0.088141',
+    maleLifeExpectancy: '6.33',
+    femaleDeathProb: '0.066584',
+    femaleLifeExpectancy: '7.45'
+  },
+  {
+    age: '85',
+    maleDeathProb: '0.097854',
+    maleLifeExpectancy: '5.89',
+    femaleDeathProb: '0.074258',
+    femaleLifeExpectancy: '6.95'
+  },
+  {
+    age: '86',
+    maleDeathProb: '0.108747',
+    maleLifeExpectancy: '5.48',
+    femaleDeathProb: '0.083053',
+    femaleLifeExpectancy: '6.47'
+  },
+  {
+    age: '87',
+    maleDeathProb: '0.120919',
+    maleLifeExpectancy: '5.08',
+    femaleDeathProb: '0.093123',
+    femaleLifeExpectancy: '6.01'
+  },
+  {
+    age: '88',
+    maleDeathProb: '0.134425',
+    maleLifeExpectancy: '4.71',
+    femaleDeathProb: '0.104540',
+    femaleLifeExpectancy: '5.57'
+  },
+  {
+    age: '89',
+    maleDeathProb: '0.149273',
+    maleLifeExpectancy: '4.37',
+    femaleDeathProb: '0.117305',
+    femaleLifeExpectancy: '5.16'
+  },
+  {
+    age: '90',
+    maleDeathProb: '0.165452',
+    maleLifeExpectancy: '4.05',
+    femaleDeathProb: '0.131392',
+    femaleLifeExpectancy: '4.78'
+  },
+  {
+    age: '91',
+    maleDeathProb: '0.182935',
+    maleLifeExpectancy: '3.75',
+    femaleDeathProb: '0.146753',
+    femaleLifeExpectancy: '4.43'
+  },
+  {
+    age: '92',
+    maleDeathProb: '0.201679',
+    maleLifeExpectancy: '3.48',
+    femaleDeathProb: '0.163331',
+    femaleLifeExpectancy: '4.11'
+  },
+  {
+    age: '93',
+    maleDeathProb: '0.221637',
+    maleLifeExpectancy: '3.23',
+    femaleDeathProb: '0.181064',
+    femaleLifeExpectancy: '3.81'
+  },
+  {
+    age: '94',
+    maleDeathProb: '0.242747',
+    maleLifeExpectancy: '3.01',
+    femaleDeathProb: '0.199886',
+    femaleLifeExpectancy: '3.55'
+  },
+  {
+    age: '95',
+    maleDeathProb: '0.263672',
+    maleLifeExpectancy: '2.81',
+    femaleDeathProb: '0.218908',
+    femaleLifeExpectancy: '3.31'
+  },
+  {
+    age: '96',
+    maleDeathProb: '0.284014',
+    maleLifeExpectancy: '2.64',
+    femaleDeathProb: '0.237815',
+    femaleLifeExpectancy: '3.09'
+  },
+  {
+    age: '97',
+    maleDeathProb: '0.303355',
+    maleLifeExpectancy: '2.49',
+    femaleDeathProb: '0.256265',
+    femaleLifeExpectancy: '2.90'
+  },
+  {
+    age: '98',
+    maleDeathProb: '0.321268',
+    maleLifeExpectancy: '2.36',
+    femaleDeathProb: '0.273894',
+    femaleLifeExpectancy: '2.73'
+  },
+  {
+    age: '99',
+    maleDeathProb: '0.337332',
+    maleLifeExpectancy: '2.24',
+    femaleDeathProb: '0.290328',
+    femaleLifeExpectancy: '2.58'
+  },
+  {
+    age: '100',
+    maleDeathProb: '0.354198',
+    maleLifeExpectancy: '2.12',
+    femaleDeathProb: '0.307747',
+    femaleLifeExpectancy: '2.42'
+  },
+  {
+    age: '101',
+    maleDeathProb: '0.371908',
+    maleLifeExpectancy: '2.01',
+    femaleDeathProb: '0.326212',
+    femaleLifeExpectancy: '2.28'
+  },
+  {
+    age: '102',
+    maleDeathProb: '0.390503',
+    maleLifeExpectancy: '1.90',
+    femaleDeathProb: '0.345785',
+    femaleLifeExpectancy: '2.14'
+  },
+  {
+    age: '103',
+    maleDeathProb: '0.410029',
+    maleLifeExpectancy: '1.80',
+    femaleDeathProb: '0.366532',
+    femaleLifeExpectancy: '2.01'
+  },
+  {
+    age: '104',
+    maleDeathProb: '0.430530',
+    maleLifeExpectancy: '1.70',
+    femaleDeathProb: '0.388524',
+    femaleLifeExpectancy: '1.88'
+  },
+  {
+    age: '105',
+    maleDeathProb: '0.452057',
+    maleLifeExpectancy: '1.60',
+    femaleDeathProb: '0.411835',
+    femaleLifeExpectancy: '1.76'
+  },
+  {
+    age: '106',
+    maleDeathProb: '0.474659',
+    maleLifeExpectancy: '1.51',
+    femaleDeathProb: '0.436546',
+    femaleLifeExpectancy: '1.65'
+  },
+  {
+    age: '107',
+    maleDeathProb: '0.498392',
+    maleLifeExpectancy: '1.42',
+    femaleDeathProb: '0.462738',
+    femaleLifeExpectancy: '1.54'
+  },
+  {
+    age: '108',
+    maleDeathProb: '0.523312',
+    maleLifeExpectancy: '1.34',
+    femaleDeathProb: '0.490503',
+    femaleLifeExpectancy: '1.44'
+  },
+  {
+    age: '109',
+    maleDeathProb: '0.549478',
+    maleLifeExpectancy: '1.26',
+    femaleDeathProb: '0.519933',
+    femaleLifeExpectancy: '1.34'
+  },
+  {
+    age: '110',
+    maleDeathProb: '0.576951',
+    maleLifeExpectancy: '1.18',
+    femaleDeathProb: '0.551129',
+    femaleLifeExpectancy: '1.24'
+  },
+  {
+    age: '111',
+    maleDeathProb: '0.605799',
+    maleLifeExpectancy: '1.10',
+    femaleDeathProb: '0.584196',
+    femaleLifeExpectancy: '1.15'
+  },
+  {
+    age: '112',
+    maleDeathProb: '0.636089',
+    maleLifeExpectancy: '1.03',
+    femaleDeathProb: '0.619248',
+    femaleLifeExpectancy: '1.06'
+  },
+  {
+    age: '113',
+    maleDeathProb: '0.667893',
+    maleLifeExpectancy: '0.96',
+    femaleDeathProb: '0.656403',
+    femaleLifeExpectancy: '0.98'
+  },
+  {
+    age: '114',
+    maleDeathProb: '0.701288',
+    maleLifeExpectancy: '0.90',
+    femaleDeathProb: '0.695787',
+    femaleLifeExpectancy: '0.91'
+  },
+  {
+    age: '115',
+    maleDeathProb: '0.736353',
+    maleLifeExpectancy: '0.84',
+    femaleDeathProb: '0.736353',
+    femaleLifeExpectancy: '0.84'
+  },
+  {
+    age: '116',
+    maleDeathProb: '0.773170',
+    maleLifeExpectancy: '0.78',
+    femaleDeathProb: '0.773170',
+    femaleLifeExpectancy: '0.78'
+  },
+  {
+    age: '117',
+    maleDeathProb: '0.811829',
+    maleLifeExpectancy: '0.72',
+    femaleDeathProb: '0.811829',
+    femaleLifeExpectancy: '0.72'
+  },
+  {
+    age: '118',
+    maleDeathProb: '0.852420',
+    maleLifeExpectancy: '0.66',
+    femaleDeathProb: '0.852420',
+    femaleLifeExpectancy: '0.66'
+  },
+  {
+    age: '119',
+    maleDeathProb: '0.895041',
+    maleLifeExpectancy: '0.61',
+    femaleDeathProb: '0.895041',
+    femaleLifeExpectancy: '0.61'
+  }
+];
+
